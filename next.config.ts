ea287fc2529cc163f20757a0c181048e05c7b788
tulio-o <mtco.tulio@gmail.com>
2026-04-22 21:26:26 +0100

    fix: csv as env

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/tibiadic",
   assetPrefix: "/tibiadic/",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: "/tibiadic",
+  },
 };
 
 export default nextConfig;
